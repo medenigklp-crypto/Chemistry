9f3bb4c824403d77cc039ed12a4ae487c7f11259
medenigklp-crypto <medeni.gklp@gmail.com>
2026-06-26 21:43:49 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,13 @@
+// js/data.js
+const EL_DATA = { ... }; // (tam veri çok uzun, sana kısaltılmış haliyle vereyim)
+
+const ELS = [
+  {n:1, sym:"H", name:"Hidrojen", mass:1.008, cat:"Ametal"},
+  {n:2, sym:"He", name:"Helyum", mass:4.0026, cat:"Soy Gaz"},
+  // ... tüm elementler
+];
+
+const PT_POS = { /* pozisyonlar */ };
+const CAT_COLORS2 = { /* renkler */ };
+const TREND_DATA = { /* trend verileri */ };
+const TREND_CONFIG = { /* trend konfigürasyonu */ };
